af3af05fc30fdf50988e638c3e85e4ec55562481
Tristan Elliott <>
2021-02-06 01:38:01 -0400

    second controller

--- src/main/java/tacos/TacoCloudApplication.java
+++ src/main/java/tacos/TacoCloudApplication.java
@@ -3,11 +3,11 @@ package tacos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication  // Signifies that this is a Spring Boot application
 public class TacoCloudApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TacoCloudApplication.class, args);
+		SpringApplication.run(TacoCloudApplication.class, args); // RUNS THE APPLICATION
 	}
 
 }
